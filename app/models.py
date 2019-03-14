e161dd0520dc5ce2b3b1b0d6bfae6e28cdfe2c52
mfs98 <mfs98@ua.pt>
2019-03-14 11:58:23 +0000

    new project

---
+++ app/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+'''
+class Receita(models.Model):
+    nome = models.CharField(max_length=70)
+    descricao = models.CharField(max_length=500)
+    preparacao = models.CharField(max_length=3000)
+    tempo = models.IntegerField()
+    dificuldade = models.CharField(max_length=10)
+    dose = models.IntegerField()
+    imagem = models.URLField()
+    data = models.DateField()
+    #utilizador
+
+    def __str__(self):
+        return self.nome
+
+
+class Ingredientes(models.Model):
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE())
+    ingredienteName = models.CharField(max_length=100)
+    ingredienteQuant = models.DecimalField()
+
+    def __str__(self):
+        return self.ingredienteName + self.ingredienteQuant
+
+
+class Tags(models.Model):
+    nome = models.CharField(max_length=50)
+    receitas = models.ManyToManyField(Receita)
+
+    def __str__(self):
+        return self.nome
+
+
+class Comentario(models.Model):
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE())
+    comentario = models.CharField(max_length=200)
+    data = models.DateTimeField()
+    #utilizador
+
+    def __str__(self):
+        return self.comentario
+
+'''
---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title': 'Home Page',
+        'year': datetime.now().year,
+    }
+    return render(request, 'index.html', tparams)
+
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title': 'Contact',
+        'message': 'Your contact page.',
+        'year': datetime.now().year,
+    }
+    return render(request, 'contact.html', tparams)
+
+
+def about(request):
+    assert isinstance(request, HttpRequest)
+    tparams = {
+        'title': 'About',
+        'message': 'Your application description page.',
+        'year': datetime.now().year,
+    }
+    return render(request, 'about.html', tparams)
