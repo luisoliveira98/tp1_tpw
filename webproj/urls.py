fd38a9440745bb88e1411e94f41571e6e05e81a7
mfs98 <mfs98@ua.pt>
2019-03-16 13:41:28 +0000

    Login

--- webproj/urls.py
+++ webproj/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
