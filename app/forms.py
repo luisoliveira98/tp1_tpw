15a4590cb30560e16fdfa9f645777aa454996e61
luisoliveira98 <luisoliveira98@ua.pt>
2019-03-18 14:42:52 +0000

    Portfolio_v1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,7 @@
 from django import forms
+
+
+class AdicionarReceita(forms.Form):
+    nome = forms.CharField(label="Nome:", max_length=70)
+    descricao = forms.CharField(label="Descrição:", max_length=500, widget=forms.Textarea)
+    preparacao =
