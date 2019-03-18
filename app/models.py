d6fba0b6e3fe1d3e74e00b12064141ef7330ce56
luisoliveira98 <luisoliveira98@ua.pt>
2019-03-18 14:09:58 +0000

    database

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-'''
+
 class Receita(models.Model):
     nome = models.CharField(max_length=70)
     descricao = models.CharField(max_length=500)
@@ -10,16 +10,16 @@ class Receita(models.Model):
     dose = models.IntegerField()
     imagem = models.URLField()
     data = models.DateField()
-    #utilizador
+    utilizador = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
 
 class Ingredientes(models.Model):
-    receita = models.ForeignKey(Receita, on_delete=models.CASCADE())
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE)
     ingredienteName = models.CharField(max_length=100)
-    ingredienteQuant = models.DecimalField()
+    ingredienteQuant = models.FloatField()
 
     def __str__(self):
         return self.ingredienteName + self.ingredienteQuant
@@ -34,12 +34,10 @@ class Tags(models.Model):
 
 
 class Comentario(models.Model):
-    receita = models.ForeignKey(Receita, on_delete=models.CASCADE())
+    receita = models.ForeignKey(Receita, on_delete=models.CASCADE)
     comentario = models.CharField(max_length=200)
     data = models.DateTimeField()
-    #utilizador
+    utilizador = models.CharField(max_length=100)
 
     def __str__(self):
         return self.comentario
-
-'''
